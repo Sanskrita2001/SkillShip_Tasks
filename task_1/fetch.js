403a64dba93f5061774c4623eb04bfdb5286a4ad
Sanskrita Saha <60420648+Sanskrita2001@users.noreply.github.com>
2020-10-13 23:01:20 +0530

    Fetch

---
+++ task_1/fetch.js
@@ -0,0 +1,14 @@
+const fetch = require("node-fetch");
+const url = "https://api.github.com";
+
+const getData = async (url) => {
+  try {
+    const response = await fetch(url);
+    const json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getData(url);
